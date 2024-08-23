9b9ca85eac0e36d59b0e0f3001ac58337e3c2758
Daiara Barroso <160050745+daiaraaa@users.noreply.github.com>
2024-08-22 23:10:21 -0300

    Update BankAccount.js

--- BankAccount.js
+++ BankAccount.js
@@ -1,32 +1,36 @@
 class BankAccount {
     #balance
     #accountHolder
-    constructor(accountHolder) {
-        this.#balance = 0
-        this.#accountHolder = accountHolder
+  
+    constructor(accountHolder, balance) {
+      this.#balance = balance += 0 
+      this.#accountHolder = accountHolder
     }
-
+  
     deposit(amount) {
-        this.#balance += amount
-        console.log(`${this.#accountHolder} depositou R$${amount.toFixed(2)} | Saldo total: R$${this.#balance.toFixed(2)}`)
+      this.#balance += amount
+      console.log(`${this.#accountHolder} depositou R$${amount.toFixed(2)} | Saldo total: R$${this.#balance.toFixed(2)}`)
     }
 
+  
     withdraw(amount) {
-        if (this.#balance >= amount) {
-            this.#balance -= amount
-            console.log(`${this.#accountHolder} sacou R$${amount.toFixed(2)} | Saldo total: R$${this.#balance.toFixed(2)}`)
-            console.log("=========================================================")
-        } else {
-            console.log('Saldo insuficiente! Você está sem saldo :(')
-        }
+      if (this.#balance >= amount) {
+        this.#balance -= amount
+        console.log(`${this.#accountHolder} sacou R$${amount.toFixed(2)} | Saldo total: R$${this.#balance.toFixed(2)}`)
+        return true
+      } else {
+        console.log(`Saldo insuficiente para ${this.#accountHolder}`)
+        return false
+      }
     }
-
+  
     getBalance() {
-        return this.#balance
+      return this.#balance
     }
-
+  
     getAccountHolder() {
-        return this.#accountHolder
+      return this.#accountHolder
     }
-}
-module.exports = BankAccount
+  }
+  
+  module.exports = BankAccount
